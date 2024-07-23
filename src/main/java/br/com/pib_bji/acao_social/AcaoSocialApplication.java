e3ab3f5577d7ff4d27a76cd6efca021118dfd1bf
Sávio Emerick <savio.ea@hotmail.com>
2024-07-23 00:00:02 -0300

    first commit

---
+++ src/main/java/br/com/pib_bji/acao_social/AcaoSocialApplication.java
@@ -0,0 +1,13 @@
+package br.com.pib_bji.acao_social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcaoSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcaoSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pib_bji/acao_social/AcaoSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pib_bji.acao_social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcaoSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
